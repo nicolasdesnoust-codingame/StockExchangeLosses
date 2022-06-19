8a4a4a3d6c5335f76b0edb2409d68844a356f0f5
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2022-06-20 00:28:36 +0200

    refactor: make business logic more explicit and remove unused code

--- domain/loss.go
+++
@@ -1,3 +0,0 @@
-package domain
-
-type Loss int
--- domain/stock_value.go
+++ domain/stock_value.go
@@ -6,6 +6,7 @@ import (
 )
 
 type StockValue int
+
 type StockValues struct {
 	Content []StockValue
 }
@@ -20,23 +21,27 @@ func (stockValues *StockValues) Size() int {
 
 func NewStockValues(rawStockValues []int) *StockValues {
 	var stockValues StockValues
-	log.Println(rawStockValues)
 
 	for _, rawStockValue := range rawStockValues {
 		stockValues.Append(StockValue(rawStockValue))
 	}
-	log.Println(stockValues.Content)
 
 	return &stockValues
 }
 
 func (stockValues *StockValues) FindMaximalLoss() int {
-	losses := make([]int, 0)
-	log.Println(stockValues.Content)
+	minimums := stockValues.findMinimumAmongFollowersForEachStockValue()
 
-	// Parcourir le tableau dans le sens inverse et en remplir un nouveau avec à chaque case le minimum observé dans la séquence
+	losses := stockValues.findMaximalLossForEachStockValue(minimums)
+
+	maximalLoss := findMaximumPositiveValueIn(losses)
+	return -maximalLoss
+}
+
+func (stockValues *StockValues) findMinimumAmongFollowersForEachStockValue() []int {
 	minimums := make([]int, stockValues.Size()-1)
 	currentMinimum := math.MaxInt32
+
 	for stockValueIndex := stockValues.Size() - 1; stockValueIndex > 0; stockValueIndex-- {
 		if int(stockValues.Content[stockValueIndex]) < currentMinimum {
 			currentMinimum = int(stockValues.Content[stockValueIndex])
@@ -44,20 +49,31 @@ func (stockValues *StockValues) FindMaximalLoss() int {
 
 		minimums[stockValueIndex-1] = currentMinimum
 	}
+
 	log.Println(minimums)
+	return minimums
+}
+
+func (stockValues *StockValues) findMaximalLossForEachStockValue(minimums []int) []int {
+	losses := make([]int, 0)
 
 	for stockValueIndex := 0; stockValueIndex < stockValues.Size()-1; stockValueIndex++ {
-		minimumAmongFollowingValues := minimums[stockValueIndex]
-
-		losses = append(losses, int(stockValues.Content[stockValueIndex])-int(minimumAmongFollowingValues))
+		minimumAmongFollowers := minimums[stockValueIndex]
+		loss := int(stockValues.Content[stockValueIndex]) - int(minimumAmongFollowers)
+		losses = append(losses, loss)
 	}
 
-	maximalLoss := 0
-	for _, loss := range losses {
-		if loss > maximalLoss {
-			maximalLoss = loss
+	return losses
+}
+
+func findMaximumPositiveValueIn(array []int) int {
+	maximum := 0
+
+	for _, item := range array {
+		if item > maximum {
+			maximum = item
 		}
 	}
 
-	return -maximalLoss
+	return maximum
 }
